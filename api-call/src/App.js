0ab9c3b8f8a905cbd4a8a188eabfc76e7b269d56
Samaneasyeat <156658674+Samaneasyeat@users.noreply.github.com>
2026-03-02 10:42:51 +0530

    react fetch API with loading and error

--- api-call/src/App.js
+++ api-call/src/App.js
@@ -3,11 +3,29 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setData(data));
+    async function fetchData() {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!res.ok) {
+          throw new Error("http response ");
+        }
+        const result = await res.json();
+        setData(result);
+      } catch (error) {
+        // This block catches network errors or the error thrown above
+        console.error("Error fetching data:", error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
   }, []);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p style={{ color: "red" }}>Error: {error}</p>; // Display user-friendly error
   return (
     <div className="App">
       {data.map((u) => (
